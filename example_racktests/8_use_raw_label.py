ac4570d99694b2c7e6f073c21d6e42cc168c442a
Shlomi Matichin <shlomi@stratoscale.com>
2014-11-12 11:01:23 +0200

    Cold restart tested; using raw label tested

---
+++ example_racktests/8_use_raw_label.py
@@ -0,0 +1,12 @@
+from strato.racktest.infra.suite import *
+import subprocess
+
+ROOTFS_LABEL = subprocess.check_output([
+    "solvent", "printlabel", "--product=rootfs", "--repositoryBasename=rootfs-basic"])
+
+
+class Test:
+    HOSTS = dict(it=dict(rootfs=ROOTFS_LABEL))
+
+    def run(self):
+        TS_ASSERT("hello" in host.it.ssh.run.script("echo hello"))
--- py/strato/racktest/infra/rootfslabel.py
+++ py/strato/racktest/infra/rootfslabel.py
@@ -1,6 +1,6 @@
-import subprocess
 from upseto import gitwrapper
 from upseto import run
+import subprocess
 
 
 class RootfsLabel:
@@ -11,6 +11,9 @@ class RootfsLabel:
                 "solvent", "printlabel", "--thisProject", "--product=rootfs"]).strip()
             wrapper = gitwrapper.GitWrapper(".")
             self._hint = wrapper.originURLBasename()
+        elif self._labelExists(self._rootfs):
+            self._label = self._rootfs
+            self._hint = self._rootfs
         else:
             self._label = run.run([
                 "solvent", "printlabel", "--repositoryBasename", rootfs, "--product=rootfs"]).strip()
@@ -21,3 +24,7 @@ class RootfsLabel:
 
     def imageHint(self):
         return self._hint
+
+    def _labelExists(self, label):
+        with open("/dev/null", "w") as out:
+            return subprocess.call(["solvent", "labelexists", "--label", label], stdout=out, stderr=out) == 0
